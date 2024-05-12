ad6f8d29acb63d26eb26f5f73eef077c7ed8ee4d
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-05-12 23:32:46 +0700

    test

---
+++ app.py
@@ -0,0 +1,14 @@
+"""for a"""
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route and a view function
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
