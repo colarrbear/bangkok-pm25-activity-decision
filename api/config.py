88e6bbb0f87a3ca73b9a81038567177ce7296c34
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-05-13 06:47:45 +0700

    test3

---
+++ api/config.py
@@ -0,0 +1,4 @@
+DB_HOST = "iot.cpe.ku.ac.th"
+DB_USER = "b6510545756"
+DB_PASSWD = "suparak.ar@ku.th"
+DB_NAME = "group09"
--- api/pair_the_data.py
+++ api/pair_the_data.py
@@ -18,35 +18,52 @@ pool = PooledDB(
     blocking=True,
 )
 
+# # Load data from the database and CSV file
+# def get_data_from_db():
+#     conn = pool.connection()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM AQMTHAI")
+#     data = cursor.fetchall()
+#     cursor.close()
+#     conn.close()
+#     return data
+#
+# def get_response_from_csv():
+#     with open('response_clean.csv', 'r') as f:
+#         # convert datetime to same format as in the database
+#         data = pd.read_csv(f)
+#         data['timestamp'] = pd.to_datetime(data['timestamp']).dt.strftime('%Y-%m-%d %H:%M:%S')
+#         data = data.to_dict(orient='records')
+#     return data
+#
+# # Pair the data from the database and CSV file
+# def pair_data():
+#     data = get_data_from_db()
+#     response = get_response_from_csv()
+#     paired_data = []
+#     for d in data:
+#         for r in response:
+#             if d[1] == r['timestamp']:
+#                 paired_data.append((d[0], r['timestamp'], r['value'], r['response']))
+#     return paired_data
+#
+#
+# # print to check the output
+# print(pair_data())
+
 # Load data from the database and CSV file
-def get_data_from_db():
-    conn = pool.connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM AQMTHAI")
-    data = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return data
+db_data = pd.read_sql_query("SELECT * FROM AQMTHAI", pool.connection())
+csv_data = pd.read_csv("response_clean.csv")
 
-def get_response_from_csv():
-    with open('response_clean.csv', 'r') as f:
-        # convert datetime to same format as in the database
-        data = pd.read_csv(f)
-        data['timestamp'] = pd.to_datetime(data['timestamp']).dt.strftime('%Y-%m-%d %H:%M:%S')
-        data = data.to_dict(orient='records')
-    return data
+# Convert CSV timestamps to a common format
+csv_df = pd.DataFrame(csv_data)
+csv_df["datetime"] = pd.to_datetime(csv_df["datetime"], format="%d/%m/%Y, %H:%M:%S")
+csv_df["datetime"] = csv_df["datetime"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
-# Pair the data from the database and CSV file
-def pair_data():
-    data = get_data_from_db()
-    response = get_response_from_csv()
-    paired_data = []
-    for d in data:
-        for r in response:
-            if d[1] == r['timestamp']:
-                paired_data.append((d[0], r['timestamp'], r['value'], r['response']))
-    return paired_data
+# Convert database timestamps to a common format (in this case, already in ISO 8601 format)
+db_df = pd.DataFrame(db_data)
 
+# Merge datasets based on timestamp
+merged_df = pd.merge(csv_df, db_df, on="datetime")
 
-# print to check the output
-print(pair_data())
+print(merged_df)
